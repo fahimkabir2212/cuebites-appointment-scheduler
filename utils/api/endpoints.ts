0df2cfe95efde0e641f2fa58445ccfb6755d7b23
A K M Fahim Kabir <fahimkabir2213@gmail.com>
2026-01-05 23:41:09 +0600

    Added booking endpoints

--- utils/api/endpoints.ts
+++ utils/api/endpoints.ts
@@ -4,3 +4,14 @@ export const staffEndpoints = {
   getStaffList: `${API_BASE_URL}/api/staff`,
   createStaff: `${API_BASE_URL}/api/staff`,
 };
+
+export const bookingEndpoints = {
+  getBookingList: `${API_BASE_URL}/api/bookings`,
+  getBookingDetails: (bookingId: string) =>
+    `${API_BASE_URL}/api/bookings/${bookingId}`,
+  updateBookingDetails: (bookingId: string) =>
+    `${API_BASE_URL}/api/bookings/${bookingId}`,
+  deleteBookingDetails: (bookingId: string) =>
+    `${API_BASE_URL}/api/bookings/${bookingId}`,
+  createBooking: `${API_BASE_URL}/api/bookings`,
+};
